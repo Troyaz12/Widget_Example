2bc7ab0505967e83e78655e65eefdbcf0e4bbf42
Troy Gugler <guglert80@gmail.com>
2017-11-19 16:48:09 -0700

    feat: Updated widgetview add activity
    updated listview to say hello widget and added the activity for the
    widget

---
+++ app/src/main/java/tegprogramming/com/widget_example/Widget_Main_Activity.java
@@ -0,0 +1,34 @@
+package tegprogramming.com.widget_example;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by TroysMacBook on 11/18/17.
+ */
+
+public class Widget_Main_Activity extends AppWidgetProvider {
+
+
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
+
+
+
+
+
+
+
