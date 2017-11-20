9a294dac00fab0484b39400921be6ae24b14b57b
Troy Gugler <guglert80@gmail.com>
2017-11-19 18:33:04 -0700

    feat: Update manifest to add service
    updated the manifest to add the widget service, also updated the widget_main_activity
    initialize the remote views.

--- app/src/main/java/tegprogramming/com/widget_example/Widget_Main_Activity.java
+++ app/src/main/java/tegprogramming/com/widget_example/Widget_Main_Activity.java
@@ -4,6 +4,8 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
 
 /**
  * Created by TroysMacBook on 11/18/17.
@@ -22,8 +24,32 @@ public class Widget_Main_Activity extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
+
+        for (int widgetId : appWidgetIds) {
+            RemoteViews mView = initViews(context, appWidgetManager, widgetId);
+            appWidgetManager.updateAppWidget(widgetId, mView);
+        }
+
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
+
+    private RemoteViews initViews(Context context,
+                                  AppWidgetManager widgetManager, int widgetId) {
+
+        RemoteViews mView = new RemoteViews(context.getPackageName(),
+                R.layout.widget_main_activity);
+
+        Intent intent = new Intent(context, WidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        mView.setRemoteAdapter(widgetId, R.id.widgetCollectionList, intent);
+
+        return mView;
+    }
+
+
+
 }
 
 
